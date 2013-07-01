518bbb3c631b202a78eae77e1b0f8de0f5d6b7f0
asgeir <asgeir@asgeir-Latitude-E4310.(none)>
2013-07-01 11:24:37 +0200

    klassestruktur, første utkast

---
+++ script.rb
@@ -0,0 +1,29 @@
+class Review
+
+	def initialize(uri)
+		@uri = uri
+		find_book
+	end
+
+	private
+
+	def find_book
+		# TODO SPARQL query
+	end
+
+end
+
+class Book
+	def intialize(uri)
+		@uri = uri
+	end
+end
+
+class Resource
+	def intialize(uri)
+		@uri = uri
+	end
+end
+
+class Bokkilden < Resource
+end
