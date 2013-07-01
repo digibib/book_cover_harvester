3a1c9293380cb7172e447179cba0ca5671dbe170
Asgeir Rekkavik <asgeir@asgeir-Latitude-E4310.(none)>
2013-07-01 13:40:33 +0200

    Setup repository

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "rdf"
+gem "rdf-virtuoso"
+gem "faraday"
--- script.rb
+++ script.rb
@@ -1,5 +1,33 @@
-class Review
+require "rdf"
+require "rdf/virtuoso"
+require "faraday"
+require "./vocabularies.rb"
+require "./settings.rb"
 
+sparql_endpoint = "http://marc2rdf.deichman.no/sparql"
+sparql_update_endpoint = "http://marc2rdf.deichman.no/sparql-auth"
+username = "rdfstore"
+password = "rdf99store"
+
+REPO = RDF::Virtuoso::Repository.new(Settings::SPARQL_ENDPOINT,
+									:update_uri => Settings::SPARQL_UPDATE_ENDPOINT,
+									:username => Settings::USERNAME,
+									:password => Settings::PASSWORD,
+									:auth_method => 'digest')
+QUERY = RDF::Virtuoso::Query
+
+q = QUERY.select(:review).distinct
+q.where([:book, RDF::REV.hasReview, :review],)
+q.where([:book, RDF.type, RDF::BIBO.Document])
+q.minus([:book, RDF::FOAF.depiction, :cover_url])
+
+res =  REPO.select(q)
+
+res.bindings
+
+
+
+class Review
 	def initialize(uri)
 		@uri = uri
 		find_book
@@ -10,7 +38,6 @@ class Review
 	def find_book
 		# TODO SPARQL query
 	end
-
 end
 
 class Book
