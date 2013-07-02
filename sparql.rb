2abb60a400b6e2040388e38addff71cf8f6bae84
Asgeir Rekkavik <asgeir@asgeir-Latitude-E4310.(none)>
2013-07-02 09:33:08 +0200

    Wrote fetch_reviews and fetch_book_data queries

--- sparql.rb
+++ sparql.rb
@@ -14,11 +14,20 @@ module Sparql
 	QUERY = RDF::Virtuoso::Query
 
 	def self.fetch_reviews_without_depiction
-		q = QUERY.select(:review).distinct
+		q = QUERY.select(:review).distinct.from(Settings::BOOKSGRAPH)
 		q.where([:book, RDF::REV.hasReview, :review],)
 		q.where([:book, RDF.type, RDF::BIBO.Document])
 		q.minus([:book, RDF::FOAF.depiction, :cover_url])
 		REPO.select(q).bindings[:review]
+	end
+
+	def self.fetch_book_data(review_uri)
+		review_uri = RDF::URI(review_uri) unless review_uri.instance_of?(RDF::URI)
+		q = QUERY.select(:book, :isbn).distinct.from(Settings::BOOKSGRAPH)
+		q.where([:book, RDF::REV.hasReview, review_uri])
+		q.where([:book, RDF.type, RDF::BIBO.Document])
+		q.where([:book, RDF::BIBO.isbn, :isbn])
+		REPO.select(q)
 	end
 
 end
