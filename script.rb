d9f985e7ea6d6c46e5aec53acbeb7d6870545127
Asgeir Rekkavik <asgeir@asgeir-Latitude-E4310.(none)>
2013-07-02 10:07:07 +0200

    Bokkilden request and result parsing

--- script.rb
+++ script.rb
@@ -33,13 +33,15 @@ class Resource
 		prepare_request
 	end
 
-	def prepare_request
-		nil
-	end
-
 	def check_for_cover_url
 		result = Faraday.get(@path+@query_params)
 		parse_result(result)
+	end
+
+	private
+
+	def prepare_request
+		nil
 	end
 
 	def parse_result(result)
@@ -50,13 +52,16 @@ end
 
 class Bokkilden < Resource
 
+	private
+
 	def prepare_request
 		@path = "http://partner.bokkilden.no/SamboWeb/partner.do"
 		@query_params = "?rom=MP&format=XML&uttrekk=5&pid=0&ept=3&xslId=117&antall=10&enkeltsok=#{@isbn}&profil=partner&sort=popularitet&order=DESC"
 	end
 
 	def parse_result(result)
-
-
+		xml = Nokogiri::XML(result.body)
+		xml.xpath('//BildeURL'). map { |url| url.content}
+	end
 
 end
