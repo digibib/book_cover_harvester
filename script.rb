2a3e45ae4c605f4d5313b1978cf07f2ad5f64d57
Asgeir Rekkavik <asgeir@asgeir-Latitude-E4310.(none)>
2013-07-02 09:34:17 +0200

    methods for fetching data from external sources

--- script.rb
+++ script.rb
@@ -1,31 +1,62 @@
 require "faraday"
 require "./sparql.rb"
+require "nokogiri"
 
 
 class Review
 	def initialize(uri)
 		@uri = uri
-		find_book
+		@books = Array(find_books(@uri))
 	end
 
 	private
 
-	def find_book
-		# TODO SPARQL query
+	def find_books(uri)
+		books = Sparql.fetch_book_data(uri)
+		books.map { |book|
+			Book.new(book.book, book.isbn)
+		}
 	end
 end
 
 class Book
-	def intialize(uri)
+	def initialize(uri, isbn)
 		@uri = uri
+		@isbn = isbn
 	end
 end
 
 class Resource
-	def intialize(uri)
-		@uri = uri
+
+	def initialize(isbn)
+		@isbn = isbn
+		prepare_request
 	end
+
+	def prepare_request
+		nil
+	end
+
+	def check_for_cover_url
+		result = Faraday.get(@path+@query_params)
+		parse_result(result)
+	end
+
+	def parse_result(result)
+		raise NotImplementedError
+	end
+
 end
 
 class Bokkilden < Resource
+
+	def prepare_request
+		@path = "http://partner.bokkilden.no/SamboWeb/partner.do"
+		@query_params = "?rom=MP&format=XML&uttrekk=5&pid=0&ept=3&xslId=117&antall=10&enkeltsok=#{@isbn}&profil=partner&sort=popularitet&order=DESC"
+	end
+
+	def parse_result(result)
+
+
+
 end
